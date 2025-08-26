2be8265a0afd2e647bac37e75e31325df90172f7
TalalBnoob <79761207+TalalBalnoob@users.noreply.github.com>
2025-08-26 13:23:40 +0300

    add a sidebar for navigation

---
+++ src/components/NavigationSideBar.tsx
@@ -0,0 +1,47 @@
+import { Home, LocalOffer, Receipt } from '@mui/icons-material'
+import { Link } from '@tanstack/react-router'
+import type { ReactNode } from 'react'
+
+const NavigationSideBar = () => {
+	return (
+		<nav className='flex w-1/6 flex-col bg-gray-900 px-4 text-white'>
+			<h1 className='m-4 text-4xl'>Stockly</h1>
+			<div className='mt-10 flex flex-col items-start gap-3'>
+				<Link
+					className='w-full'
+					to='/'>
+					<Item
+						text='Home'
+						icon={<Home />}
+					/>
+				</Link>
+				<Link
+					className='w-full'
+					to='.'>
+					<Item
+						text='Products'
+						icon={<LocalOffer />}
+					/>
+				</Link>
+				<Link
+					className='w-full'
+					to='.'>
+					<Item
+						text='Orders'
+						icon={<Receipt />}
+					/>
+				</Link>
+			</div>
+		</nav>
+	)
+}
+
+const Item = ({ icon, text }: { icon: ReactNode; text: string }) => {
+	return (
+		<div className='flex w-full cursor-pointer items-center gap-2 rounded p-4 text-start text-white hover:bg-gray-100/5'>
+			{icon} {text}
+		</div>
+	)
+}
+
+export default NavigationSideBar
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,26 +1,17 @@
-import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
+import { createRootRoute, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 import { ToastContainer } from 'react-toastify'
+import NavigationSideBar from '../components/NavigationSideBar'
 
 export const Route = createRootRoute({
 	component: () => (
 		<>
-			<div className='flex gap-2 p-2'>
-				<Link
-					to='/'
-					className='[&.active]:font-bold'>
-					Home
-				</Link>{' '}
-				<Link
-					to='/about'
-					className='[&.active]:font-bold'>
-					About
-				</Link>
+			<div className='flex h-screen w-screen'>
+				<NavigationSideBar />
+				<section className='mx-auto w-5/6 bg-gray-100'>
+					<Outlet />
+				</section>
 			</div>
-			<hr />
-			<section className='container mx-auto p-4'>
-				<Outlet />
-			</section>
 			<ToastContainer position='bottom-right' />
 			<TanStackRouterDevtools />
 		</>
