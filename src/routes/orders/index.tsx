8ebb7e688a70935c2c822548aba91b625f04ecb2
TalalBnoob <79761207+TalalBalnoob@users.noreply.github.com>
2025-08-27 11:42:24 +0300

    add real data fetching

--- src/routes/orders/index.tsx
+++ src/routes/orders/index.tsx
@@ -1,6 +1,10 @@
 import Paper from '@mui/material/Paper'
 import { createFileRoute } from '@tanstack/react-router'
-import { DataGrid, type GridRowsProp, type GridColDef } from '@mui/x-data-grid'
+import { DataGrid, type GridColDef } from '@mui/x-data-grid'
+import { useQuery } from '@tanstack/react-query'
+import { getOrders } from '../../services/api/orders'
+import { useEffect, useState } from 'react'
+import type { Order } from '../../types'
 
 export const Route = createFileRoute('/orders/')({
 	component: RouteComponent,
@@ -8,54 +12,33 @@ export const Route = createFileRoute('/orders/')({
 
 const columns: GridColDef[] = [
 	{ field: 'id', headerName: 'ID', flex: 0.5 },
-	{ field: 'name', headerName: 'Product Name', flex: 2 },
-	{ field: 'description', headerName: 'Description', flex: 2 },
-	{ field: 'price', headerName: 'Price', flex: 1 },
-	{ field: 'quantity', headerName: 'Quantity', flex: 1 },
-	{ field: 'total', headerName: 'Total', flex: 1 },
+	{ field: 'customer_Name', headerName: 'Customer Name', flex: 2 },
+	{ field: 'customer_Contact', headerName: 'Customer Contact', flex: 2 },
+	{ field: 'totel_amount', headerName: 'Total Amount', flex: 1 },
 	{ field: 'status', headerName: 'Status', flex: 1 },
 	{ field: 'createdAt', headerName: 'Created At', flex: 1 },
 ]
 
-// TODO: replace with actual data
-const rows: GridRowsProp = [
-	{
-		id: 1,
-		name: 'Data Grid',
-		description: 'the Community version',
-		price: 100,
-		quantity: 1,
-		total: 100,
-		status: 'pending',
-		createdAt: '2021-01-01',
-	},
-	{
-		id: 2,
-		name: 'Data Grid Pro',
-		description: 'the Pro version',
-		price: 200,
-		quantity: 2,
-		total: 400,
-		status: 'pending',
-		createdAt: '2021-01-01',
-	},
-	{
-		id: 3,
-		name: 'Data Grid Premium',
-		description: 'the Premium version',
-		price: 300,
-		quantity: 3,
-		total: 900,
-		status: 'pending',
-		createdAt: '2021-01-01',
-	},
-]
-
 function RouteComponent() {
+	const [items, setItems] = useState<Order[]>([])
+
+	const query = useQuery({
+		queryKey: ['orders'],
+		queryFn: getOrders,
+	})
+
+	useEffect(() => {
+		if (query.data && query.isSuccess) {
+			setItems(query.data)
+		}
+	}, [query.data, query.isSuccess])
+
+	if (query.isLoading) return <div>Loading...</div>
+
 	return (
 		<Paper sx={{ height: '100%', width: '100%' }}>
 			<DataGrid
-				rows={rows}
+				rows={items}
 				columns={columns}
 				initialState={{ pagination: { paginationModel: { pageSize: 10 } } }}
 				pageSizeOptions={[5, 10]}
